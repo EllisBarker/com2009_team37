bce4eca76746b538f4eea79d5ed9eedfb0cb07e6
EllisBarker <ellisbarker2003@gmail.com>
2023-03-25 14:20:22 +0000

    Robot now moves in a circle. Measurements for linear and angular velocity and zeroing of display readings need work.

---
+++ src/task1_odom_subscriber.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import rospy, math
+from nav_msgs.msg import Odometry
+from tf.transformations import euler_from_quaternion
+
+class OdomSubscriber():
+    def __init__(self):
+        node_name = "odom_subscriber"
+        rospy.init_node(node_name, anonymous=True)
+        self.zero_reading_found = False
+        self.init_position = []
+        self.sub = rospy.Subscriber("odom", Odometry, self.callback)
+        rospy.loginfo(f"Task 1: '{node_name}' node is active...")
+
+    def callback(self, topic_data: Odometry):
+        pose = topic_data.pose.pose
+        position = pose.position
+        orientation = pose.orientation 
+
+        if self.zero_reading_found == False:
+            self.init_position = [position.x, position.y, 
+                                 (euler_from_quaternion([orientation.x, orientation.y, 
+                                                         orientation.z, orientation.w], 
+                                                         'sxyz')[2])*(180/math.pi)]
+            self.zero_reading_found = True
+
+        print(f"x={(position.x - abs(self.init_position[0])):.2f} [m], y={(position.y - abs(self.init_position[1])):.2f} [m], yaw={((euler_from_quaternion([orientation.x, orientation.y, orientation.z, orientation.w], 'sxyz')[2]) *(180/math.pi) - abs(self.init_position[2])):.1f} [degrees]")
+
+    def main_loop(self):
+        rospy.spin()
+
+if __name__ == '__main__':
+    subscriber_instance = OdomSubscriber()
+    subscriber_instance.main_loop()
---
+++ src/task1_publisher_figure_eight.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import rospy, math
+from geometry_msgs.msg import Twist 
+
+class FigureEightPublisher():
+    def __init__(self):
+        self.node_name = "figure_eight_publisher"
+        topic_name = "cmd_vel"
+        self.pub = rospy.Publisher(topic_name, Twist, queue_size=10)
+        rospy.init_node(self.node_name, anonymous=True)
+        self.rate = rospy.Rate(1) 
+        self.ctrl_c = False 
+        self.current_loop = 1
+        rospy.on_shutdown(self.shutdown)
+        rospy.loginfo(f"Task 1: '{self.node_name}' node is active...") 
+
+    def shutdown(self):
+        print(f"Stopping the '{self.node_name}' task 1 node at: {rospy.get_time()}")
+        vel_cmd = Twist()
+        vel_cmd.linear.x = 0.0 # m/s
+        vel_cmd.angular.z = 0.0 # rad/s
+        self.ctrl_c = True
+
+    def main_loop(self):
+        while not self.ctrl_c:
+            vel_cmd = Twist()
+            vel_cmd.linear.x = 0.10471975512 # m/s ((pi * 1 metre diameter) / 30 seconds)
+            if self.current_loop == 1:
+                vel_cmd.angular.z = - (2 * math.pi) / 30 # rad/s
+            elif self.current_loop == 2:
+                vel_cmd.angular.z = (2 * math.pi) / 30 # rad/s
+            # CONDITIONAL HERE TO CHANGE THE VALUE OF CURRENT LOOP DEPENDING ON SOMETHING
+            self.pub.publish(vel_cmd)
+            self.rate.sleep()
+
+if __name__ == '__main__': 
+    publisher_instance = FigureEightPublisher() 
+    try:
+        publisher_instance.main_loop() 
+    except rospy.ROSInterruptException:
+        pass
