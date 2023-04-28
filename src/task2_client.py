f57a496db25cc6eca89d2577455f1e6a7fe3cc00
EllisBarker <ellisbarker2003@gmail.com>
2023-04-28 12:03:26 +0100

    Skeleton code for task 2 client

--- src/task2_client.py
+++ src/task2_client.py
@@ -1,2 +1,26 @@
 #!/usr/bin/env python3
 
+import rospy
+import actionlib
+
+from tuos_ros_msgs.msg import SearchAction, SearchGoal, SearchFeedback
+
+class SearchActionClient():
+    goal = SearchGoal()
+
+    def __init__(self):
+        pass
+
+    def feedback_callback(self, feedback_data: SearchFeedback):
+        pass
+
+    def shutdown_ops(self):
+        pass
+
+    def main_loop(self):
+        self.goal.approach_distance = 0.4 # metres
+        self.goal_fwd_velocity = 0.15 # m/s
+        
+if __name__ == '__main__':
+    node = SearchActionClient()
+    node.main_loop()
