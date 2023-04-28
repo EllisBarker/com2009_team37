b8b4b189b67dc4605469f88a3dd927e018381c21
TUoS Student <student@TENB88584C305A7.shefuniad.shef.ac.uk>
2023-04-28 22:44:57 +0100

    Added loop logic for task 2 server

--- src/task2_server.py
+++ src/task2_server.py
@@ -2,6 +2,7 @@
 
 import rospy
 import actionlib
+import random # Please ask the lecturer if we can use this library
 from tuos_ros_msgs.msg import SearchAction, SearchFeedback, SearchResult, SearchGoal
 from tb3 import Tb3Move, Tb3Odometry, Tb3LaserScan
 from math import sqrt, pow
@@ -34,7 +35,7 @@ class SearchActionServer():
         self.closest_object = self.tb3_lidar.min_distance
         self.closest_object_location = self.tb3_lidar.closest_object_position
 
-        self.vel_controller.set_move_cmd(vel, 0.0)
+        # self.vel_controller.set_move_cmd(vel, 0.0)
 
         # while true:
             # if not close:
@@ -43,6 +44,30 @@ class SearchActionServer():
                 # publish velocity commands, update values
                 ###(CHANGE ANGULAR VELOCITY (also make linear 0) AND TIME OF ROTATION)
 
+        ################# Minh's code (Modify if needed)
+
+        # Get start time (ideally 0s)
+        start_time = rospy.get_rostime() 
+        # Robot runs for 85 seconds
+        while (rospy.get_rostime().secs - start_time.secs) < 85: 
+            # While the distance between the robot and the obstacle is more than the threshold, move forward
+            if self.closest_object > goal.approach_distance: 
+                self.vel_controller.set_move_cmd(vel, 0.0)
+            # Stop the robot, make it rotate and move forward after the distance between the 
+            # robot and the obstacle is smaller than the threshold
+            else:
+                # Random duration of turning, ideally ranged between 1 to 3 seconds to avoid wasting time on rotating
+                random_duration = random.randrange(1,3) 
+                # Capture the moment that the robot stops when it's close to an obstacle
+                time_stopped = rospy.get_rostime()
+                # Make it rotate for that duration, with a random positive angular velocity
+                while (rospy.get_rostime().secs - time_stopped.secs) < random_duration:
+                    self.vel_controller.set_move_cmd(0.0, random.random()) 
+                # Set angular velocity back to 0 after finish rotating and keep moving forward
+                self.vel_controller.set_move_cmd(vel, 0.0)
+
+            
+        ################# End of Minh's code
         if success:
             rospy.loginfo("Search completed successfully.")
 
