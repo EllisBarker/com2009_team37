f26b2893b31d215345bea32b2c7058c32d1f8eba
TUoS Student <student@TENB885849FDE8D.shefuniad.shef.ac.uk>
2023-05-04 14:02:23 +0100

    Updated task2 client and server, currently working

--- src/task2_client.py
+++ src/task2_client.py
@@ -1,40 +1,155 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
+# task2_client.py
 
+from tb3 import Tb3LaserScan, Tb3Move, Tb3Odometry
 import rospy
 import actionlib
 
 from tuos_ros_msgs.msg import SearchAction, SearchGoal, SearchFeedback
+from math import pi
 
 class SearchActionClient():
     goal = SearchGoal()
 
+    def feedback_callback(self, feedback_data: SearchFeedback):
+        # Print all the data from TB3
+        # Get the current distance travelled, from the feedback message
+        # and assign this to a class variable...
+        self.distance = feedback_data.current_distance_travelled
+        if self.msg_counter > 5:
+            print(f"Feedback: Distance travelled = {self.distance:.3f} meters.")
+            print(f"Robot Yaw: {self.tb3_odom.yaw} degrees.")
+            self.msg_counter = 0
+        else:
+            self.msg_counter += 1
+
+        # turning 
+        # goasl must be different
+        self.turnRight = True
+        if 0<self.tb3_scan.min_distance< 0.5 and self.turnRight:
+            self.goal.approach_distance = 0.05 # m 
+            self.goal.fwd_velocity = -0.1 # m/s
+            self.client.send_goal(self.goal, feedback_cb=self.feedback_callback)
+            self.stopTurnRight = True
+            self.turnRight = False
+        # moving 
+        if self.msg_counter2 > 2.5:
+            self.goal.approach_distance = 0.1 # m 
+            self.goal.fwd_velocity = 0.15 # m/s
+            self.client.send_goal(self.goal, feedback_cb=self.feedback_callback)
+            self.msg_counter2 = 0
+            self.turnRight = True
+        else:
+            self.msg_counter2 += 1
+
+
     def __init__(self):
+        self.distance = 0.0
+        self.msg_counter = 0
+        self.msg_counter2 = 0
+
         self.action_complete = False
         rospy.init_node("search_action_client")
         self.rate = rospy.Rate(1)
 
-        self.client = actionlib.SimpleActionClient("/task_2_search", SearchAction)
+        ## TODO: setup a "simple action client" with a callback function
+        ## and wait for the server to be available...
+        self.client = actionlib.SimpleActionClient(
+            "/minh_search_server",
+            SearchAction
+        )
         self.client.wait_for_server()
-        rospy.on_shutdown(self.shutdown_ops)
+        
+        # create the instance of tb3 functions
+        self.tb3_move = Tb3Move()
+        self.tb3_odom = Tb3Odometry()
+        self.tb3_scan = Tb3LaserScan()
 
-    def feedback_callback(self, feedback_data: SearchFeedback):
-        pass
+
+        rospy.on_shutdown(self.shutdown_ops)
 
     def shutdown_ops(self):
         if not self.action_complete:
-            rospy.logwarn("Received shutdown request. Cancelling...")
+            rospy.logwarn("Received a shutdown request. Cancelling Goal...")
+            ## TODO: cancel the goal request, if this node is shutdown before the action has completed...
             self.client.cancel_goal()
-            rospy.logwarn("Cancelled...")
+            rospy.logwarn("Goal Cancelled...")
 
-        rospy.sleep(1)
+        ## TODO: Print the result here...
+        rospy.sleep(1) # Wait for the result to come in 
         result = self.client.get_result()
-        # print results here if need be?
+        print("RESULT:")
+        print(f"  * Action State = {self.client.get_state()}")
+        print(f"  * Total Distance Travelled = {result.total_distance_travelled:.3f} meters")
+        print(f"  * Closest Object Distance = {result.closest_object_distance:.3f} meters")
+        print(f"  * Closest Object Angle = {result.closest_object_angle:.1f} degrees.")
+
+
 
     def main_loop(self):
-        self.goal.approach_distance = 0.4 # metres
-        self.goal.fwd_velocity = 0.15 # m/s
+        ## TODO: assign values to all goal parameters
+        ## and send the goal to the action server...
+        self.goal.approach_distance = 0.4 # m 
+        self.goal.fwd_velocity = 0.1 # m/s
         self.client.send_goal(self.goal, feedback_cb=self.feedback_callback)
-        
+
+
+        # FOR ASSIGNMENT: This is the part for making the robot runs continuously (instead of stopping after not detecting any obstacle within 2 meters like the code below)
+        while self.client.get_state() < 2:
+            ## TODO: Construct an if statement and cancel the goal if the 
+            ## distance travelled exceeds 2 meters...
+            
+            #useless
+            # if self.distance > 2:
+            #     print("STOP: Distance exceeded 2 meters!!!")
+            #     # break out of the while loop to stop the node:
+            #     break
+
+            self.rate.sleep()
+
+        self.action_complete = True if self.client.get_state() == 3 else False
+
 if __name__ == '__main__':
+    ## TODO: Instantiate the node and call the main_loop() method from it...
     node = SearchActionClient()
     node.main_loop()
+
+### LEGACY CODE
+# import rospy
+# import actionlib
+
+# from tuos_ros_msgs.msg import SearchAction, SearchGoal, SearchFeedback
+
+# class SearchActionClient():
+#     goal = SearchGoal()
+
+#     def __init__(self):
+#         self.action_complete = False
+#         rospy.init_node("search_action_client")
+#         self.rate = rospy.Rate(1)
+
+#         self.client = actionlib.SimpleActionClient("/task_2_search", SearchAction)
+#         self.client.wait_for_server()
+#         rospy.on_shutdown(self.shutdown_ops)
+
+#     def feedback_callback(self, feedback_data: SearchFeedback):
+#         pass
+
+#     def shutdown_ops(self):
+#         if not self.action_complete:
+#             rospy.logwarn("Received shutdown request. Cancelling...")
+#             self.client.cancel_goal()
+#             rospy.logwarn("Cancelled...")
+
+#         rospy.sleep(1)
+#         result = self.client.get_result()
+#         # print results here if need be?
+
+#     def main_loop(self):
+#         self.goal.approach_distance = 0.4 # metres
+#         self.goal.fwd_velocity = 0.15 # m/s
+#         self.client.send_goal(self.goal, feedback_cb=self.feedback_callback)
+        
+# if __name__ == '__main__':
+#     node = SearchActionClient()
+#     node.main_loop()
--- src/task2_server.py
+++ src/task2_server.py
@@ -1,10 +1,18 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
+# task2_server.py
 
+# Import the core Python modules for ROS and to implement ROS Actions:
 import rospy
 import actionlib
-import random # Please ask the lecturer if we can use this library
+import random
+
+# Import all the necessary ROS message types:
 from tuos_ros_msgs.msg import SearchAction, SearchFeedback, SearchResult, SearchGoal
+
+# Import the tb3 modules from tb3.py
 from tb3 import Tb3Move, Tb3Odometry, Tb3LaserScan
+
+# Import some other useful Python Modules
 from math import sqrt, pow
 
 class SearchActionServer():
@@ -12,67 +20,189 @@ class SearchActionServer():
     result = SearchResult()
 
     def __init__(self):
-        self.actionserver = actionlib.SimpleActionServer("/task_2_search", SearchAction, self.action_server_launcher, auto_start=False)
+        # Create a "simple action server" with a callback function, and start it...
+        self.actionserver = actionlib.SimpleActionServer(
+            "/minh_search_server", 
+            SearchAction, 
+            self.action_server_launcher, 
+            auto_start=False
+        )
         self.actionserver.start()
 
-        # useful publisher/subscriber functions from tb3
+        # pull in some useful publisher/subscriber functions from the tb3.py module:
         self.vel_controller = Tb3Move()
         self.tb3_odom = Tb3Odometry()
         self.tb3_lidar = Tb3LaserScan()
 
         rospy.loginfo("The 'Search Action Server' is active...")
 
-    # the 'callback' function of the server
+    # The action's "callback function":
     def action_server_launcher(self, goal: SearchGoal):
         rate = rospy.Rate(10)
 
+        ## TODO: Implement some checks on the "goal" input parameter(s)
         success = True
-        vel = goal.fwd_velocity
-        dist = goal.approach_distance
+        vel = goal.fwd_velocity # m/s
+        dist = goal.approach_distance # m
 
+        # if vel > 0.26 or vel < 0:
+        #     print("Invalid Velocity!")
+        #     success = False
+        # if dist < 0.2:
+        #     print("Invalid Distance!")
+        #     success = False
+
+
+        if not success:
+            # Abort the action server if an invalid goal has been requested...
+            self.result.total_distance_travelled = -1
+            self.result.closest_object_distance = -1
+            self.result.closest_object_angle = -1
+            self.actionserver.set_aborted(self.result)
+            return
+
+        # Print a message to indicate that the requested goal was valid
+        print(f"Search goal received: fwd_velocity = {vel} m/s, approach_distance = {dist} m.")
+
+        # Get the robot's current odometry from the Tb3Odometry() class:
         self.posx0 = self.tb3_odom.posx
         self.posy0 = self.tb3_odom.posy
+        # Get information about objects up ahead from the Tb3LaserScan() class:
         self.closest_object = self.tb3_lidar.min_distance
         self.closest_object_location = self.tb3_lidar.closest_object_position
 
-        # self.vel_controller.set_move_cmd(vel, 0.0)
-
-        # while true:
-            # if not close:
-                # update values and publish feedback
-            # else if close:
-                # publish velocity commands, update values
-                ###(CHANGE ANGULAR VELOCITY (also make linear 0) AND TIME OF ROTATION)
-
-        ################# Minh's code (Modify if needed)
-
-        # Get start time (ideally 0s)
-        start_time = rospy.get_rostime() 
-        # Robot runs for 85 seconds
-        while (rospy.get_rostime().secs - start_time.secs) < 85: 
-            # While the distance between the robot and the obstacle is more than the threshold, move forward
-            if self.closest_object > goal.approach_distance: 
-                self.vel_controller.set_move_cmd(vel, 0.0)
-            # Stop the robot, make it rotate and move forward after the distance between the 
-            # robot and the obstacle is smaller than the threshold
+        ## TODO: set the robot's forward velocity (as specified in the "goal")...
+        if goal.fwd_velocity<0:
+            self.v = random.random()
+            if self.v > 0.9:
+                self.vel_controller.set_move_cmd(linear=0,angular=1.0)
             else:
-                # Random duration of turning, ideally ranged between 1 to 3 seconds to avoid wasting time on rotating
-                random_duration = random.randrange(1,3) 
-                # Capture the moment that the robot stops when it's close to an obstacle
-                time_stopped = rospy.get_rostime()
-                # Make it rotate for that duration, with a random positive angular velocity
-                while (rospy.get_rostime().secs - time_stopped.secs) < random_duration:
-                    self.vel_controller.set_move_cmd(0.0, random.random()) 
-                # Set angular velocity back to 0 after finish rotating and keep moving forward
-                self.vel_controller.set_move_cmd(vel, 0.0)
+                self.vel_controller.set_move_cmd(linear=0,angular=-1.0)
+        else:
 
-            
-        ################# End of Minh's code
+            self.vel_controller.set_move_cmd(linear=vel,angular=0)
+
+        ## TODO: establish a conditional statement so that the  
+        ## while loop continues as long as the distance to the closest object
+        ## ahead of the robot is always greater than the "approach distance"
+        ## (as specified in the "goal")...
+        while self.tb3_lidar.min_distance > dist:
+            # update LaserScan data:
+            self.closest_object = self.tb3_lidar.min_distance
+            self.closest_object_location = self.tb3_lidar.closest_object_position
+
+            # Publish a velocity command to make the robot start moving 
+            self.vel_controller.publish()
+
+            # determine how far the robot has travelled so far:
+            self.distance = sqrt(pow(self.posx0 - self.tb3_odom.posx, 2) + pow(self.posy0 - self.tb3_odom.posy, 2))
+
+            self.result.total_distance_travelled = self.distance
+            self.result.closest_object_distance = self.closest_object
+            self.result.closest_object_angle = self.closest_object_location
+            # check if there has been a request to cancel the action mid-way through:
+            if self.actionserver.is_preempt_requested():
+                # Take appropriate action if the action is cancelled (pre-empted)...
+                print("Pre-empt requested!")
+                self.actionserver.set_preempted(self.result)
+                self.vel_controller.stop()
+                success = False
+                # Exit the loop:
+                break
+
+            # Update all feedback message values and publish a feedback message:
+            self.feedback.current_distance_travelled = self.distance
+            self.actionserver.publish_feedback(self.feedback)
+
+            rate.sleep()
+
         if success:
-            rospy.loginfo("Search completed successfully.")
+            rospy.loginfo("approach completed successfully.")
+            ## TODO: Set the action server to "succeeded" and stop the robot...
+            self.vel_controller.stop()
+            self.actionserver.set_succeeded(self.result)
+
 
 
 if __name__ == '__main__':
     rospy.init_node("search_action_server")
     SearchActionServer()
     rospy.spin()
+
+### LEGACY CODE
+# import rospy
+# import actionlib
+# import random # Please ask the lecturer if we can use this library
+# from tuos_ros_msgs.msg import SearchAction, SearchFeedback, SearchResult, SearchGoal
+# from tb3 import Tb3Move, Tb3Odometry, Tb3LaserScan
+# from math import sqrt, pow
+
+# class SearchActionServer():
+#     feedback = SearchFeedback() 
+#     result = SearchResult()
+
+#     def __init__(self):
+#         self.actionserver = actionlib.SimpleActionServer("/task_2_search", SearchAction, self.action_server_launcher, auto_start=False)
+#         self.actionserver.start()
+
+#         # useful publisher/subscriber functions from tb3
+#         self.vel_controller = Tb3Move()
+#         self.tb3_odom = Tb3Odometry()
+#         self.tb3_lidar = Tb3LaserScan()
+
+#         rospy.loginfo("The 'Search Action Server' is active...")
+
+#     # the 'callback' function of the server
+#     def action_server_launcher(self, goal: SearchGoal):
+#         rate = rospy.Rate(10)
+
+#         success = True
+#         vel = goal.fwd_velocity
+#         dist = goal.approach_distance
+
+#         self.posx0 = self.tb3_odom.posx
+#         self.posy0 = self.tb3_odom.posy
+#         self.closest_object = self.tb3_lidar.min_distance
+#         self.closest_object_location = self.tb3_lidar.closest_object_position
+
+#         # self.vel_controller.set_move_cmd(vel, 0.0)
+
+#         # while true:
+#             # if not close:
+#                 # update values and publish feedback
+#             # else if close:
+#                 # publish velocity commands, update values
+#                 ###(CHANGE ANGULAR VELOCITY (also make linear 0) AND TIME OF ROTATION)
+
+#         ################# Minh's code (Modify if needed)
+
+#         # Get start time (ideally 0s)
+#         start_time = rospy.get_rostime() 
+#         # Robot runs for 85 seconds
+#         while (rospy.get_rostime().secs - start_time.secs) < 85: 
+#             # While the distance between the robot and the obstacle is more than the threshold, move forward
+#             if self.closest_object > dist: 
+#                 self.vel_controller.set_move_cmd(vel, 0.0)
+#             # Stop the robot, make it rotate and move forward after the distance between the 
+#             # robot and the obstacle is smaller than the threshold
+#             else:
+#                 # Random duration of turning, ideally ranged between 1 to 3 seconds to avoid wasting time on rotating
+#                 random_duration = random.randrange(1,3) 
+#                 # Capture the moment that the robot stops when it's close to an obstacle
+#                 time_stopped = rospy.get_rostime()
+#                 # Make it rotate for that duration, with a random positive angular velocity
+#                 while (rospy.get_rostime().secs - time_stopped.secs) < random_duration:
+#                     self.vel_controller.set_move_cmd(0.0, random.random()) 
+#                 # Set angular velocity back to 0 after finish rotating and keep moving forward
+#                 self.vel_controller.set_move_cmd(vel, 0.0)
+
+            
+#         ################# End of Minh's code
+#         if success:
+#             rospy.loginfo("Search completed successfully.")
+
+
+# if __name__ == '__main__':
+#     rospy.init_node("search_action_server")
+#     SearchActionServer()
+#     rospy.spin()
