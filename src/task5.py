4994c58dd41dc270211789060b95d3ebeaab9b80
EllisBarker <ellisbarker2003@gmail.com>
2023-05-11 14:26:45 +0100

    Task 5 camera stuff, task 4 launch file rename and made the python file executable

--- src/task5.py
+++ src/task5.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import rospy
+import rospy, roslaunch
 
 # Imaging modules
 import cv2
@@ -10,17 +10,20 @@ from sensor_msgs.msg import Image
 # Movement and sensor modules
 from tb3 import Tb3Move, Tb3Odometry, Tb3LaserScan
 
-# Need SLAM modules??????
-
 class Exploration():
     def __init__(self):
         node_name = "exploration"
         rospy.init_node(node_name)
 
         # Camera-related functionality
-        self.camera_subscriber = rospy.Subscriber("/camera/rgb/image_raw",
+        # "/camera/rgb/image_raw" for simulation
+        self.camera_topic = "/camera/rgb/image_raw"
+        self.camera_subscriber = rospy.Subscriber(self.camera_topic,
             Image, self.camera_callback)
         self.cvbridge_interface = CvBridge()
+
+        # Map-realted functionality
+        self.map_path = "com2009_team37/maps/task5_map"
 
         self.robot_controller = Tb3Move()
 
@@ -28,7 +31,7 @@ class Exploration():
         self.ctrl_c = False
         rospy.on_shutdown(self.shutdown_ops)
 
-        self.rate = rospy.Rate(5)
+        self.rate = rospy.Rate(100)
         
         # Colour-related values
         self.m00 = 0
@@ -41,6 +44,16 @@ class Exploration():
         cv2.destroyAllWindows()
         self.ctrl_c = True
 
+    def save_image(self):
+        rospy.init_node("map_getter", anonymous=True)
+        launch = roslaunch.scriptapi.ROSLaunch()
+        launch.start()
+        print(f"Saving map at time: {rospy.get_time()}...")
+        node = roslaunch.core.Node(package="map_server",
+                                   node_type="map_saver",
+                                   args=f"-f {self.map_path}")
+        process = launch.launch(node)
+
     def camera_callback(self, img_data):
         try:
             cv_img = self.cvbridge_interface.imgmsg_to_cv2(img_data, desired_encoding="bgr8")
@@ -50,7 +63,7 @@ class Exploration():
 
     def main(self):
         while not self.ctrl_c:
-            pass
+            self.save_image()
             # IF DESIRED OBJECT DETECTED, TURN AND TRY TO APPROACH DETECTED
 
 if __name__ == '__main__':
