97ce10a551c8282727519b113f5c8d4a0a3c8e6c
Jack Burke <jburke5@sheffield.ac.uk>
2023-05-13 05:59:11 +0100

    Created a basic maze solver for Task 3

---
+++ src/task3_maze_navigation.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+import rospy
+from tb3 import Tb3Move, Tb3Odometry, Tb3LaserScan
+import waffle
+from math import sqrt
+
+class MazeNavigation():
+    def __init__(self):
+        self.node_name = "maze_navigation"
+
+        rospy.init_node(self.node_name, anonymous=True)
+        rate = rospy.Rate(2) # hz
+
+        self.vel_controller = Tb3Move()
+        self.tb3_lidar = Tb3LaserScan()
+        self.tb3_odm = Tb3Odometry()
+
+        self.lin_vel = 0.0
+        self.ang_vel = 0.0
+        self.direction = 0
+        self.movement = "move_fwd"
+        self.transition = True
+
+        self.ctrl_c = False 
+        rospy.sleep(0.1)
+
+        rospy.on_shutdown(self.shutdown)
+        rospy.loginfo(f"Task 3: '{self.node_name}' node is active...") 
+
+    def shutdown(self):
+        print(f"Stopping the '{self.node_name}' task 3 node.")
+        self.vel_controller.set_move_cmd(linear=0, angular=0)
+        self.ctrl_c = True
+
+    def main_loop(self):
+        while not self.ctrl_c:
+
+
+            print(self.tb3_lidar.distance)
+
+            left_wall_rate  = self.tb3_lidar.distance.l3 - self.tb3_lidar.distance.l4
+            right_wall_rate = self.tb3_lidar.distance.r3 - self.tb3_lidar.distance.r4
+
+            if self.transition:
+                self.vel_controller.set_move_cmd(linear=0, angular=0) # stop
+                # reset all values
+                yaw_ref = self.tb3_odm.yaw
+                xpos_ref = self.tb3_odm.posx
+                ypos_ref = self.tb3_odm.posy
+                current_yaw = 0.0
+                current_distance = 0.0
+                self.transition = False
+                print(f"Entering state: {self.movement}")
+            elif self.movement == "turn":
+                current_yaw = current_yaw + abs(self.tb3_odm.yaw - yaw_ref)
+                yaw_ref = self.tb3_odm.yaw
+
+                print("Turning: Rotating",["90 left!","90 right!","180 dead end!"][self.direction])
+
+                if current_yaw >= [90, 90, 180][self.direction]:
+                    self.movement = "move_fwd"
+                    self.transition = True
+                else:
+                    self.vel_controller.set_move_cmd(linear=0.0, angular=[0.2,-0.2, 0.2][self.direction])
+
+                print(f"{current_yaw=}")
+
+            elif self.movement == "move_fwd":
+                    current_distance = current_distance + sqrt(pow(self.tb3_odm.posy-ypos_ref,2) + pow(self.tb3_odm.posx-xpos_ref,2))
+                    xpos_ref = self.tb3_odm.posx
+                    ypos_ref = self.tb3_odm.posy
+                    
+                    if self.tb3_lidar.min_distance < 0.42: # if obstacle infront
+                        
+                        # if walls on both sides - dead end - turn 180
+                        if  self.tb3_lidar.distance.r4 < 0.5 and self.tb3_lidar.distance.l4 < 0.5:
+                            self.movement = "turn"
+                            self.transition = True
+                            self.direction = 2
+
+                        # if nothing on the right - turn 90 right
+                        elif self.tb3_lidar.distance.r4 > 0.8:
+                            self.movement = "turn"
+                            self.transition = True
+                            self.direction = 1
+
+                        # if nothing on the left - turn 90 left
+                        elif self.tb3_lidar.distance.l4 > 0.8:
+                            self.movement = "turn"
+                            self.transition = True
+                            self.direction = 0
+
+                    else: # move forwards    
+
+                        # prefer the right wall
+                        if abs(right_wall_rate) > 0.1:
+                            wall_rate = left_wall_rate
+                            print("Moving Forwards: Using left wall!")
+                            if abs(wall_rate) < 0.001:
+                                # forwards
+                                self.vel_controller.set_move_cmd(linear=0.2, angular=0.0)
+                            elif wall_rate < 0:
+                                # correct right
+                                self.vel_controller.set_move_cmd(linear=0.2, angular=-0.1)
+                            else:
+                                # correct left
+                                self.vel_controller.set_move_cmd(linear=0.2, angular=0.1)
+                        else:
+                            wall_rate = right_wall_rate
+                            print("Moving Forwards: Using right wall!")
+                            if abs(wall_rate) < 0.001:
+                                # forwards
+                                self.vel_controller.set_move_cmd(linear=0.2, angular=0.0)
+                            elif wall_rate < 0:
+                                # correct left
+                                self.vel_controller.set_move_cmd(linear=0.2, angular=0.1)
+                            else:
+                                # correct right
+                                self.vel_controller.set_move_cmd(linear=0.2, angular=-0.1)
+
+                    print(f"{current_distance=}")
+            
+            print(f"{right_wall_rate=:.3f}")
+            print(f"{left_wall_rate=:.3f}")
+            self.vel_controller.publish()
+            rospy.sleep(0.01)
+            
+
+if __name__ == '__main__': 
+    node = MazeNavigation() 
+    try:
+        node.main_loop() 
+    except rospy.ROSInterruptException:
+        pass
--- src/tb3.py
+++ src/tb3.py
@@ -47,16 +47,52 @@ class Tb3Odometry(object):
         return float(value) / (10**precision)
 
 class Tb3LaserScan(object):
+
+    class scanSubsets():
+        def __init__(self):
+            self.front = 0.0
+            self.r1 = 0.0; self.r2 = 0.0; self.r3 = 0.0; self.r4 = 0.0
+            self.l1 = 0.0; self.l2 = 0.0; self.l3 = 0.0; self.l4 = 0.0
+        
+        def __str__(self):
+            msg = f"              l1     front     r1          \n" \
+                  f"       l2     {self.l1:<5.3f}  {self.front:^5.3f}  {self.r1:>5.3f}     r2       \n" \
+                  f"l3     {self.l2:<5.3f}                       {self.r2:>5.3f}   r3\n" \
+                  f"{self.l3:<5.3f}                                   {self.r3:>5.3f}\n" \
+                  f"{self.l4:<5.3f} <-- l4                     r4 --> {self.r4:>5.3f}"
+            return msg
+    
     def laserscan_cb(self, scan_data):
+
+        def min_of_subset(start_index, stop_index):
+            range = np.array(scan_data.ranges[start_index: stop_index+1])
+            valid_data = range[range>0.1]
+            return valid_data.mean() if np.shape(valid_data)[0] > 0 else np.nan
+        
         left_arc = scan_data.ranges[0:21]
         right_arc = scan_data.ranges[-20:]
         front_arc = np.array(left_arc[::-1] + right_arc[::-1])
+        valid = front_arc[front_arc>0.1]
+        self.distance.front = valid.mean() if np.shape(valid)[0] > 0 else np.nan
         
         self.min_distance = front_arc.min()
         arc_angles = np.arange(-20, 21)
         self.closest_object_position = arc_angles[np.argmin(front_arc)]
 
+        # right subsets:
+        self.distance.r1 = min_of_subset(320, 340)
+        self.distance.r2 = min_of_subset(300, 320)
+        self.distance.r3 = min_of_subset(275, 290)
+        self.distance.r4 = min_of_subset(250, 265)
+        
+        # left subsets:
+        self.distance.l1 = min_of_subset(20, 40)
+        self.distance.l2 = min_of_subset(40, 60)
+        self.distance.l3 = min_of_subset(70, 85)
+        self.distance.l4 = min_of_subset(95, 110)
+
     def __init__(self):
+        self.distance = self.scanSubsets()
         self.min_distance = 0.0
         self.closest_object_position = 0.0 # degrees
-        self.subscriber = rospy.Subscriber('/scan', LaserScan, self.laserscan_cb) 
+        self.subscriber = rospy.Subscriber('/scan', LaserScan, self.laserscan_cb)
