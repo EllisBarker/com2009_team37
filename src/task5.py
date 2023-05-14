b6a5a30d423ba343b1c85f3c2d70cb87c60f7f36
EllisBarker <ellisbarker2003@gmail.com>
2023-05-14 15:23:01 +0100

    Working on task 5

--- src/task5.py
+++ src/task5.py
@@ -6,6 +6,7 @@ import rospy, roslaunch
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
+from pathlib import Path 
 
 # Movement and sensor modules
 from tb3 import Tb3Move, Tb3Odometry, Tb3LaserScan
@@ -14,6 +15,30 @@ class Exploration():
     def __init__(self):
         node_name = "exploration"
         rospy.init_node(node_name)
+        
+        # Colour and beaconing-related values
+        self.m00 = 0
+        self.m00_min = 10000
+        self.target_colour = ""
+        # RANGES SUBJECT TO CHANGE???????
+        self.colour_ranges = [["green",(40,150,100),(65,255,255)],
+                              ["blue",(115,225,100),(130,255,255)],
+                              ["red1",(0,188,100),(4,255,255)],
+                              ["red2",(175,200,100),(180,255,255)],
+                              ["yellow",(25,120,100),(35,255,255)]]
+                              #["purple",(143,153,100),(157,255,255)],
+                              #["turquoise",(84,150,100),(96,255,255)]]
+        #self.target_colour = self.colour_ranges[self.colour_ranges.index(rospy.set_param('/task5/target_colour'))]
+        for colour in self.colour_ranges:
+            if "blue" in colour:
+                self.target_colour = colour
+        self.picture_taken = False
+        self.picture_signal = False
+        self.turn_vel_fast = -0.5
+        self.turn_vel_slow = -0.1
+        self.move_rate = ""
+
+        self.rate = rospy.Rate(100)
 
         # Camera-related functionality
         # "/camera/rgb/image_raw" for simulation
@@ -24,36 +49,19 @@ class Exploration():
 
         # File-saving functionality
         self.map_path = "com2009_team37/maps/task5_map"
-        self.base_image_path = "com2009_team37/snaps/"
-        self.base_image_path.mkdir(parents=True, exist_ok=True) 
+        #self.base_image_path = Path.joinpath("com2009_team37/snaps/")
+        #self.base_image_path.mkdir(parents=True, exist_ok=True) 
 
         # Movement and sensor controllers
         self.robot_controller = Tb3Move()
         self.tb3_odom = Tb3Odometry()
         self.tb3_lidar = Tb3LaserScan()
-        
-        # Colour-related values
-        self.m00 = 0
-        self.m00_min = 10000
-        self.target_colour = rospy.set_param('/task5/target_colour')
-        # RANGES SUBJECT TO CHANGE???????
-        self.colour_ranges = [["green",(40,150,100),(65,255,255)],
-                              ["blue",(115,225,100),(130,255,255)],
-                              ["red1",(0,188,100),(4,255,255)],
-                              ["red2",(175,200,100),(180,255,255)],
-                              ["yellow",(25,120,100),(35,255,255)],
-                              ["purple",(143,153,100),(157,255,255)],
-                              ["turquoise",(84,150,100),(96,255,255)]]
-        self.picture_taken = False
-        self.picture_signal = False
 
         # Shutdown-related operations
         self.ctrl_c = False
         rospy.on_shutdown(self.shutdown_ops)
 
-        self.rate = rospy.Rate(100)
-
-        rospy.loginfo(f"TASK 5 BEACON: The target is {self.target_colour}.")
+        rospy.loginfo(f"TASK 5 BEACON: The target is {self.target_colour[0]}.")
 
     def shutdown_ops(self):
         self.robot_controller.stop()
@@ -89,7 +97,7 @@ class Exploration():
             print(e)
 
         if self.picture_signal == True:
-            self.save_picture(cv_img)
+            #self.save_picture(cv_img)
             self.picture_signal = False
             self.picture_taken = True
 
@@ -102,7 +110,7 @@ class Exploration():
         crop_img = cv_img[crop_y:crop_y+crop_height, crop_x:crop_x+crop_width]
         hsv_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
 
-        mask = cv2.inRange(hsv_img, self.colour_ranges[self.target_colour][1], self.colour_ranges[self.target_colour][2])
+        mask = cv2.inRange(hsv_img, self.target_colour[1], self.target_colour[2])
         if mask.any():
             m = cv2.moments(mask)
             self.m00 = m['m00']
@@ -112,17 +120,37 @@ class Exploration():
 
     def main(self):
         while not self.ctrl_c:
-            if self.picture_taken == False:
-                if self.m00 > self.m00_min:
-                    self.robot_controller.set_move_cmd(0.0,0.0)
-                    self.robot_controller.publish()
-                    # TURNING, ONLY SAVE IF BEACON IS STRAIGHT FORWARD
+            # Blob detected and picture has not yet been taken
+            if self.picture_taken == False and self.m00 > self.m00_min:
+                self.robot_controller.set_move_cmd(0.0,0.0)
+                self.robot_controller.publish()
+
+                # Turning to face the beacon straight ahead and take a picture
+                if self.cy >= 560-100 and self.cy <= 560+100:
+                    if self.move_rate == 'slow':
+                        self.move_rate = 'stop'
+                else:
+                    self.move_rate = 'slow'
+                
+                if self.move_rate == 'slow':
+                    print(f"MOVING SLOW: A blob of colour of size {self.m00:.0f} pixels is in view at y-position: {self.cy:.0f} pixels.")
+                    self.robot_controller.set_move_cmd(0.0, self.turn_vel_slow)
+                elif self.move_rate == 'stop' and self.stop_counter > 0:
+                    print(f"STOPPED: The blob of colour is now dead-ahead at y-position {self.cy:.0f} pixels... Counting down: {self.stop_counter}")
+                    self.robot_controller.set_move_cmd(0.0, 0.0)
                     self.picture_signal = True
+                else:
+                    print(f"MOVING SLOW: A blob of colour of size {self.m00:.0f} pixels is in view at y-position: {self.cy:.0f} pixels.")
+                    self.robot_controller.set_move_cmd(0.0, self.turn_vel_slow)
 
-            # REGULAR OBJECT AVOIDANCE
+                self.robot_controller.publish()
 
-            # RUN THIS WHEN THE CODE IS DONE
-            self.save_map()
+            else:
+                pass
+                # REGULAR OBJECT AVOIDANCE
+
+            # RUN THIS AT A REGULAR INTERVAL
+            #self.save_map()
 
 if __name__ == '__main__':
     exploration_instance = Exploration()
