3d799b59f8f4ee9772bb2e7e1715d23123671340
aca20hy <75694117+aca20hy@users.noreply.github.com>
2023-05-18 15:07:05 +0100

    Update task4_beaconing.py

--- unsorted_code/old_task4_code/task4_beaconing.py
+++ unsorted_code/old_task4_code/task4_beaconing.py
@@ -7,7 +7,7 @@ import time
 # Import some image processing modules:
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
-from laser_distance import LaserDistance
+from laser_sub import LaserSub
 # Import all the necessary ROS message types:
 from sensor_msgs.msg import Image
 import numpy as np
@@ -31,14 +31,17 @@ class colour_search(object):
         self.turn_vel_slow = -0.1
         self.target_color = None
         self.stop_flag = False
-        self.sub = LaserDistance()
+        self.sub = LaserSub()
         self.color_ranges = {
             "green": (np.array([0, 180, 0]), np.array([25, 255, 25])),
             "blue": (np.array([0, 0, 82]), np.array([25, 25, 221])),
-            "yellow": (np.array([180, 180, 0]), np.array([255, 255, 20])),
             "lightblue": (np.array([0, 82, 82]), np.array([25, 122, 122])),
         }
-
+        # self.color_ranges = {
+        #     "green": (np.array([50, 100, 100]), np.array([70, 255, 255])),
+        #     "blue": (np.array([110, 100, 100]), np.array([130, 255, 255])),
+        #     "lightblue": (np.array([165, 0, 99]), np.array([195, 250, 255])),
+        # }
         self.cvbridge_interface = CvBridge()
         self.robot_controller = Tb3Move()
         self.turn_vel_fast = -0.5
@@ -97,8 +100,6 @@ class colour_search(object):
             # Get center of target color contours
             target_color_m = cv2.moments(target_color_contours[0])
             print("m00 :",target_color_m["m00"])
-            if target_color_m["m00"]==0:
-                target_color_m["m00"]=1
             target_color_cx = int(target_color_m["m10"] / target_color_m["m00"])
             target_color_cy = int(target_color_m["m01"] / target_color_m["m00"])
             self.m00 = target_color_m["m00"]
@@ -142,27 +143,27 @@ class colour_search(object):
             if self.hsv_img is None:
                 continue
             if self.target_color is None:
-                self.move_to_cmd(True, True, 2, linear_val=0)
+                self.move(True, True, 2, linear_vel=0)
                 self.target_color = self.find_most_common_color()
                 print("TARGET DETECTED: Beaconing initiated ", self.target_color)
                 rospy.sleep(2)  # adjust time as needed
                 # turn back to original orientation
-                self.move_to_cmd(True, False, 2, linear_val=0)
+                self.move(True, False, 2, linear_vel=0)
 
                 rospy.sleep(2)  # adjust time as needed
-                self.move_to_cmd(True, True, 0.3, angular_val=0)
-
+                self.move(True, True, 0.3, angular_vel=0)
+                # self.move(True, False, move_time, linear_vel=0)
             else:
                 # Check for obstacles in front
                 # If the robot is too close to an obstacle in front, it needs to avoid it
                 # Move the robot
-                self.move_to_cmd(True, True, 0.2, angular_val=0)
+                self.move(True, True, 0.2, angular_vel=0)
                 # Check for obstacles in front
                 # If the robot is too close to an obstacle in front, it needs to avoid it
                 if self.turn_amount!=0:
                     if self.cx >= 560-80 and self.cx <= 560+80:
-                        self.move_to_cmd(True, True, 0.2, angular_val=0)
-                        if self.sub.min_front_distance< self.min_distance:
+                        self.move(True, True, 0.2, angular_vel=0)
+                        if self.sub.min_distance< self.min_distance:
                             self.robot_controller.set_move_cmd(0.0, 0.0)
                             print("BEACONING COMPLETE: The robot has now stopped.")
                             self.ctrl_c = True
@@ -171,39 +172,39 @@ class colour_search(object):
                             turn_time=abs(self.turn_amount)/13
                         else:
                             turn_time = 0.2
-                        self.move_to_cmd(True, True, turn_time, linear_val=0)
+                        self.move(True, True, turn_time, linear_vel=0)
                     elif self.turn_amount> -0.3:
                         if abs(self.turn_amount) > 2:
                             turn_time = abs(self.turn_amount) / 13
                         else:
                             turn_time = 0.2
-                        self.move_to_cmd(True, False, turn_time, linear_val=0)
-                    elif self.sub.min_front_distance< self.min_distance:
+                        self.move(True, False, turn_time, linear_vel=0)
+                    elif self.sub.min_distance< self.min_distance:
                         self.robot_controller.set_move_cmd(0.0, 0.0)
                         print("BEACONING COMPLETE: The robot has now stopped.")
                         self.ctrl_c = True
                     else:
-                        self.move_to_cmd(True, True, 0.1, angular_val=0)
-                elif self.sub.min_front_distance <= self.min_distance:
+                        self.move(True, True, 0.1, angular_vel=0)
+                elif self.sub.min_distance <= self.min_distance:
                     # Move backwards
-                    self.move_to_cmd(False, True, 0.7, angular_val=0)
+                    self.move(False, True, 0.7, angular_vel=0)
                     # If the distance on the right side is also close, adjust to the left
-                    if self.sub.min_left_distance >= self.sub.min_right_distance:
-                        self.move_to_cmd(True, True, move_time, linear_val=0)
+                    if self.sub.min_left >= self.sub.min_right:
+                        self.move(True, True, move_time, linear_vel=0)
                     # If the distance on the left side is also close, adjust to the right and turn right
-                    elif self.sub.min_left_distance <= self.sub.min_right_distance:
-                        self.move_to_cmd(True, False, move_time, linear_val=0)
-                elif self.sub.min_front_distance <= self.max_distance:
-                    if self.sub.min_left_distance >= self.sub.min_right_distance :
-                        self.move_to_cmd(True, True, move_time, linear_val=0)
+                    elif self.sub.min_left <= self.sub.min_right:
+                        self.move(True, False, move_time, linear_vel=0)
+                elif self.sub.min_distance <= self.max_distance:
+                    if self.sub.min_left >= self.sub.min_right :
+                        self.move(True, True, move_time, linear_vel=0)
                     # If the distance on the left side is also close, adjust to the right and turn right
-                    elif self.sub.min_left_distance <= self.sub.min_right_distance :
-                        self.move_to_cmd(True, False, move_time, linear_val=0)
+                    elif self.sub.min_left <= self.sub.min_right :
+                        self.move(True, False, move_time, linear_vel=0)
                 else:
                     if self.min_distance>0.8:
-                        self.move_to_cmd(True, True, 0.4, linear_val=0.7, angular_val=0)
+                        self.move(True, True, 0.4,linear_vel=0.7, angular_vel=0)
                     else:
-                        self.move_to_cmd(True, True, 0.1, angular_val=0)
+                        self.move(True, True, 0.1, angular_vel=0)
 
 
             if time.time() >= t_end:
@@ -211,24 +212,31 @@ class colour_search(object):
                 self.ctrl_c = True
 
 
-    def move_to_cmd(self, forward, left_or_right, cost_time, linear_val=0.2, angular_val=0.6):
+    def move(self, forward_backward, left_right, seconds, linear_vel=0.2, angular_vel=0.6):
         vel = Twist()
-        t_end = time.time() + cost_time
-        if forward:
-            vel.linear.x = linear_val
-            if left_or_right:
-                vel.angular.z = angular_val
+        t_end = time.time() + seconds
+        if forward_backward:
+            print("forward")
+            vel.linear.x = linear_vel
+            if left_right:
+                print("left")
+                vel.angular.z = angular_vel
             else:
-                vel.angular.z = -angular_val
+                print("right")
+                vel.angular.z = -angular_vel
         else:
-            vel.linear.x = -linear_val
-            if left_or_right:
-                vel.angular.z = -angular_val
+            print("backward")
+            vel.linear.x = -linear_vel
+            if left_right:
+                print("left")
+                vel.angular.z = -angular_vel
             else:
-                vel.angular.z = angular_val
+                print("right")
+                vel.angular.z = angular_vel
 
         while time.time() < t_end:
             self.pub.publish(vel)
+        print(seconds, "seconds have elapsed")
 
 if __name__ == '__main__':
     search_instance = colour_search()
